f37024503779db76e0f8971ebeeec4ba2c0297fa
Loukson <123784461+Loukson@users.noreply.github.com>
2024-11-06 07:16:27 +0000

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ global.location = 'Sultanpur IN'
 global.gurl = 'https://instagram.com/' // add your username
 global.devs = '22502331988';
 global.website = 'https://github.com/prabathLK/PRABATH-MD' //wa.me/+94762280384
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/H062oEI.jpeg'
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://iili.io/dmKqfTP.jpg'
 module.exports = {
   OPENAI_API_KEY:process.env.OPENAI_API_KEY || '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',
   botname: process.env.BOT_NAME || 'NARUTO-MD',
